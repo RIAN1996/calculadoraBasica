8df63b8107ec3c01bf45188567e6dc4df5153e7c
Rivera <17040044@itsuruapan.edu.mx>
2021-10-13 19:35:59 -0500

    interfaz

--- src/main/java/com/example/calculadorabasica/ControllerCalculadora.java
+++ src/main/java/com/example/calculadorabasica/ControllerCalculadora.java
@@ -1,4 +1,88 @@
 package com.example.calculadorabasica;
 
-public class ControllerCalculadora {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerCalculadora implements Initializable {
+
+    @FXML
+    private Button buttonAdd;
+
+    @FXML
+    private Button buttonClear;
+
+    @FXML
+    private Button buttonDec;
+
+    @FXML
+    private Button buttonDel;
+
+    @FXML
+    private Button buttonDiv;
+
+    @FXML
+    private Button buttonEigth;
+
+    @FXML
+    private Button buttonEqu;
+
+    @FXML
+    private Button buttonFive;
+
+    @FXML
+    private Button buttonFour;
+
+    @FXML
+    private Button buttonMult;
+
+    @FXML
+    private Button buttonNine;
+
+    @FXML
+    private AnchorPane buttonOn;
+
+    @FXML
+    private Button buttonOne;
+
+    @FXML
+    private Button buttonSeven;
+
+    @FXML
+    private Button buttonSix;
+
+    @FXML
+    private Button buttonSub;
+
+    @FXML
+    private Button buttonTrhee;
+
+    @FXML
+    private Button buttonTwo;
+
+    @FXML
+    private Button buttonZero;
+
+    @FXML
+    private TextField idTextNum;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+
+    @FXML
+    void buttonOn(ActionEvent event) {
+
+    }
+
+
 }
